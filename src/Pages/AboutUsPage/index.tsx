27f497e5acca385858738870519e77a4005aa763
LipeMachado <silvalipe765@gmail.com>
2023-08-29 23:05:06 -0300

    Add text

--- src/Pages/AboutUsPage/index.tsx
+++ src/Pages/AboutUsPage/index.tsx
@@ -4,7 +4,8 @@ import { SectionsContainers } from "../../Style/styledGlobal";
 import { FooterSectionContainer, NavBarSectionContainer } from "../HomePage/style";
 
 import {
-    AboutUsContainerSectionDiv
+    AboutUsContainerSectionDiv,
+    AboutUsTextsSectionDiv
 } from './style'
 
 export function AboutUsPage() {
@@ -17,7 +18,22 @@ export function AboutUsPage() {
             </NavBarSectionContainer>
 
             <AboutUsContainerSectionDiv>
-                <div></div>
+                <AboutUsTextsSectionDiv>
+                    <div>
+                        <h3>Bem-Vindo à Home You: Onde Lares Se Tornam Arte Interior</h3>
+                    </div>
+                    <div>
+                        <p>A Home You vai além do design de interiores, criando histórias
+                            encantadoras através de espaços cativantes. Mais do que designers,
+                            somos contadores de histórias que utilizam cores, texturas e formas
+                            como palavras para dar vida aos ambientes. Nossos projetos oferecem
+                            um lugar onde cada detalhe de design possui uma história única,
+                            onde a escolha dos móveis expressa personalidade e a funcionalidade
+                            se une à elegância de maneira harmoniosa. Bem-vindo à Home You,
+                            onde transformamos esses sonhos em realidade.
+                        </p>
+                    </div>
+                </AboutUsTextsSectionDiv>
             </AboutUsContainerSectionDiv>
 
             <FooterSectionContainer>
--- src/Pages/AboutUsPage/style.tsx
+++ src/Pages/AboutUsPage/style.tsx
@@ -3,3 +3,7 @@ import styled from "styled-components";
 export const AboutUsContainerSectionDiv = styled.div`
     height: 68vh;
 `;
+
+export const AboutUsTextsSectionDiv = styled.div`
+    display: flex;
+`;
