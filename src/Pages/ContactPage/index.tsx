fcdc64839719173cc45198a234e90d57bd73be36
LipeMachado <silvalipe765@gmail.com>
2023-09-19 11:13:09 -0300

    Create Contact Page

--- src/Pages/ContactPage/index.tsx
+++ src/Pages/ContactPage/index.tsx
@@ -1,11 +1,19 @@
+import { ArrowRight } from "phosphor-react";
+import { FooterSection } from "../../SectionsGlobals/FooterSection";
 import { NavBarSection } from "../../SectionsGlobals/NavBarSection";
 import { SectionsContainers } from "../../Style/styledGlobal";
-import { NavBarSectionContainer } from "../HomePage/style";
+import { FooterSectionContainer, NavBarSectionContainer } from "../HomePage/style";
+
 import {
     ContactContainerSectionDiv,
+    ContactFilterDiv,
+    ContactContentsSectionDiv,
+    ContactTextsContentDiv,
+    ContactFormContainerDiv
 } from "./style";
 
 export function ContactPage() {
+
     return (
         <>
             <NavBarSectionContainer>
@@ -13,9 +21,33 @@ export function ContactPage() {
                     <NavBarSection />
                 </SectionsContainers>
             </NavBarSectionContainer>
-            <ContactContainerSectionDiv>
 
+            <ContactContainerSectionDiv>
+                <ContactFilterDiv>
+                    <ContactContentsSectionDiv>
+                        <ContactTextsContentDiv>
+                            <h1>Do Conceito à Realidade: Seu Espaço, Nossa Paixão</h1>
+                            <p>
+                                Estamos ansiosos para ouvir de você.
+                                Envie-nos uma mensagem e deixe-nos
+                                transformar sua visão em realidade.
+                            </p>
+                        </ContactTextsContentDiv>
+                        <ContactFormContainerDiv>
+                            <input type="text" placeholder="Nome" />
+                            <input type="text" placeholder="Email" />
+                            <textarea placeholder="Mensagem" />
+                            <button>Enviar mensagem <ArrowRight size={22} weight="bold" /></button>
+                        </ContactFormContainerDiv>
+                    </ContactContentsSectionDiv>
+                </ContactFilterDiv>
             </ContactContainerSectionDiv>
+
+            <FooterSectionContainer>
+                <SectionsContainers>
+                    <FooterSection />
+                </SectionsContainers>
+            </FooterSectionContainer>
         </>
     )
 }
--- src/Pages/ContactPage/style.tsx
+++ src/Pages/ContactPage/style.tsx
@@ -1,6 +1,122 @@
 import styled from "styled-components";
+import ContactBackgroundPage from '../../Assets/Images/ContactBackgroundImage.png'
 
 export const ContactContainerSectionDiv = styled.div`
-    height: 86vh;
+    height: 63vh;
+    background-image: url(${ContactBackgroundPage});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
     background-color: #F9F9F9;
+
+    @media only screen and (max-width: 900px){
+        height: 100vh;
+    }
+`;
+
+export const ContactFilterDiv = styled.div`
+    backdrop-filter: brightness(30%);
+    height: 100%;
+`;
+
+export const ContactContentsSectionDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 5rem;
+    align-items: start;
+    height: 100%;
+    color: #fff;
+    padding: 5rem 1rem 1rem 1rem;
+
+    @media only screen and (max-width: 900px){
+        flex-direction: column;
+        align-items: center;
+        padding: 2rem;
+    }
+
+    @media only screen and (max-width: 550px){
+        gap: 1rem;
+    }
+`;
+
+export const ContactTextsContentDiv = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h1 {
+        font-size: 35px;
+        
+        @media only screen and (max-width: 900px){
+            text-align: center;
+        }
+    }
+
+    p {
+        width: 85%;
+        font-size: 20px;
+
+        @media only screen and (max-width: 900px){
+            width: 100%;
+            text-align: center;
+        }
+    }
+`;
+
+export const ContactFormContainerDiv = styled.div`
+    width: 450px;
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    
+    @media only screen and (max-width: 900px){
+        text-align: center;
+    }
+
+    @media only screen and (max-width: 550px){
+        width: 100%;
+    }
+
+    input {
+        width: 100%;
+        height: 40px;
+        border-radius: 2px;
+        border: none;
+        padding: 0 1rem;
+        font-size: 15px;
+        outline: none;
+        background-color: #fff;
+    }
+
+    textarea {
+        width: 100%;
+        height: 50%;
+        border-radius: 2px;
+        border: none;
+        padding: .5rem 1rem;
+        font-size: 15px;
+        outline: none;
+        background-color: #fff;
+    }
+
+    button {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+        font-size: 15px;
+        color: #fff;
+        background: transparent;
+        border: none;
+        cursor: pointer;
+
+        &:hover {
+            color: #e6e6e6;
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
 `;
